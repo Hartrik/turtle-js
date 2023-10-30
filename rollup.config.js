e859097698566dcc113a28cbb7d213858bfb677d
Patrik Harag <patrikharag@gmail.com>
2023-10-30 21:27:35 +0100

    Add minimized builds

--- rollup.config.js
+++ rollup.config.js
@@ -2,6 +2,7 @@ import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import { string } from "rollup-plugin-string";
 import image from "@rollup/plugin-image";
+import terser from '@rollup/plugin-terser';
 import pkg from './package.json';
 
 export default [
@@ -20,6 +21,22 @@ export default [
         ]
     },
 
+    // browser-friendly UMD build - minimal - MINIMIZED
+    {
+        input: 'src/dist-single/main.js',
+        output: {
+            name: 'TurtleJS',
+            file: pkg.browser_single_min,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(), // so Rollup can find libraries
+            commonjs(), // so Rollup can convert libraries to an ES modules
+
+            terser()
+        ]
+    },
+
     // browser-friendly UMD build
     {
         input: 'src/dist-all/main.js',
@@ -31,6 +48,7 @@ export default [
         plugins: [
             resolve(), // so Rollup can find libraries
             commonjs(), // so Rollup can convert libraries to an ES modules
+
             string({
                 include: "assets/*.turtle",
                 exclude: []
@@ -39,6 +57,31 @@ export default [
                 include: "assets/*.png",
                 exclude: []
             })
+        ]
+    },
+
+    // browser-friendly UMD build - MINIMIZED
+    {
+        input: 'src/dist-all/main.js',
+        output: {
+            name: 'TurtleJS',
+            file: pkg.browser_all_min,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(), // so Rollup can find libraries
+            commonjs(), // so Rollup can convert libraries to an ES modules
+
+            string({
+                include: "assets/*.turtle",
+                exclude: []
+            }),
+            image({
+                include: "assets/*.png",
+                exclude: []
+            }),
+
+            terser()
         ]
     }
 ];
